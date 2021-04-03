194bedf0fc97377726a21d8665aadb83ac55d81c
lss0815 <js815002@gmail.com>
2021-04-03 19:23:23 +0900

    image_publisher: cmake initialize

--- raspberrypi/application/image_publisher/src/main.cpp
+++ raspberrypi/application/image_publisher/src/main.cpp
@@ -12,16 +12,16 @@ int main(int, char**)
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
-    Mat edges;
-    namedWindow("edges",1);
+    //Mat edges;
+    //namedWindow("edges",1);
     for(;;)
     {
         Mat frame;
         cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, COLOR_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
+        //cvtColor(frame, edges, COLOR_BGR2GRAY);
+        //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        //Canny(edges, edges, 0, 30, 3);
+        //imshow("edges", edges);
         if(waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
