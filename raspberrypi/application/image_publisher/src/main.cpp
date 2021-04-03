cb0416e32b43bde7c64ae3e99995f1fceb37f8b3
lss0815 <js815002@gmail.com>
2021-04-03 15:59:10 +0100

    image_publisher: getting image

--- raspberrypi/application/image_publisher/src/main.cpp
+++ raspberrypi/application/image_publisher/src/main.cpp
@@ -8,21 +8,26 @@ using namespace cv;
 
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
+    VideoCapture cap;
+	cap.open(0);// open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
-
+    cap.set(CV_CAP_PROP_FPS, 5);
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     //Mat edges;
     //namedWindow("edges",1);
-    for(;;)
+    if(true)
     {
         Mat frame;
-        cap >> frame; // get a new frame from camera
+        cap.read(frame);
+		//cap >> frame; // get a new frame from camera
         //cvtColor(frame, edges, COLOR_BGR2GRAY);
         //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
         //Canny(edges, edges, 0, 30, 3);
         //imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+        //if(waitKey(30) >= 0) break;
+		imwrite("test.jpg", frame);
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
