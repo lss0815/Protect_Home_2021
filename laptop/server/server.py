a8c72e696277706eda622e7ea5c18278bdab4d6d
lss0815 <js815002@gmail.com>
2021-04-04 18:40:13 +0900

    server: fix to work

--- laptop/server/server.py
+++ laptop/server/server.py
@@ -1,15 +1,12 @@
 import zmq
 import numpy as np
-import io
-import PIL
-import sys
 import cv2 as cv
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:4000")
 
-cnt = 0
+imgCnt = 0
 
 while(True):
     buffer = socket.recv()
@@ -17,7 +14,6 @@ while(True):
     nparr = np.fromstring(buffer, np.uint8)
     img_np = cv.imdecode(nparr,  cv.IMREAD_COLOR)
 
-    cnt += 1
-    print(str(cnt) + " received")
+    print(imgCnt, " received")
 
     socket.send(b"ok")
