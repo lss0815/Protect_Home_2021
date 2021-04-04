e26057e221aca9f18133527070eaf5cb3e24f992
lss0815 <js815002@gmail.com>
2021-04-04 09:47:05 +0100

    image_publisher: fix zmq linking

--- raspberrypi/application/image_publisher/src/main.cpp
+++ raspberrypi/application/image_publisher/src/main.cpp
@@ -14,22 +14,22 @@ int main(int, char**)
         return -1;
     }
 
-    m_videoCapture.set(cv::CV_CAP_PROP_FRAME_WIDTH, 640);
-    m_videoCapture.set(cv::CV_CAP_PROP_FRAME_HEIGHT, 480);
+    m_videoCapture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    m_videoCapture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
     void *m_zmqContext = zmq_ctx_new();
     void *m_zmqRequester = zmq_socket (m_zmqContext, ZMQ_REQ);
     zmq_connect (m_zmqRequester, "tcp://192.168.60.76:4000");
 
     cv::Mat m_curFrame;
-    cv::vector<uchar> m_curBuffer;
+    std::vector<uchar> m_curBuffer;
 
     int sendCnt = 0;
     while(true)
     {
-        m_videoCapture.read(frame);
+        m_videoCapture.read(m_curFrame);
         cv::imencode(".jpg", m_curFrame, m_curBuffer);
-        zmq_send(publisher, m_curBuffer.data(), m_curBuffer.size(), ZMQ_NOBLOCK);
+        zmq_send(m_zmqRequester, m_curBuffer.data(), m_curBuffer.size(), ZMQ_NOBLOCK);
         std::cout << ++sendCnt << " image sent\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
