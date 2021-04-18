6acb8b163908282626a964fbd369252f131aa1dd
lss0815 <js815002@gmail.com>
2021-04-19 08:14:32 +0900

    android: implement array list view

--- android/app/src/main/java/com/skku/protecthome/MainActivity.java
+++ android/app/src/main/java/com/skku/protecthome/MainActivity.java
@@ -3,7 +3,10 @@ package com.skku.protecthome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.text.SimpleDateFormat;
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private ListViewAdapter adapter;
     SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +29,43 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
 
-        ListViewItem item = new ListViewItem();
-        Date date = new Date();
-        item.setDate(dateformat.format(date));
+        listView.setOnItemClickListener(
+        new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-        adapter.addItem(item);
+            }
+        });
 
-        adapter.notifyDataSetChanged();
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+
+                ListViewItem item = new ListViewItem();
+                Date date = new Date();
+                item.setDate(dateformat.format(date));
+
+                adapter.addItem(item);
+
+                adapter.notifyDataSetChanged();
+            }
+        };
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                        handler.sendEmptyMessage(0);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
     }
+
 }
