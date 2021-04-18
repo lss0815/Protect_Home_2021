daf64597ba38768c89e2b2dcdab7a1cb8c787dbc
lss0815 <js815002@gmail.com>
2021-04-18 22:10:09 +0900

    android: make initial android applciation

---
+++ android/app/src/main/java/com/skku/protecthome/ListViewAdapter.java
@@ -0,0 +1,14 @@
+package com.skku.protecthome;
+
+import android.widget.BaseAdapter;
+
+public class ListViewAdapter extends BaseAdapter {
+    priavete TextView dateTV;
+
+    private ArrayList<ListViewItem>
+
+    @Override
+    public int getCount() {
+        return list
+    }
+}
