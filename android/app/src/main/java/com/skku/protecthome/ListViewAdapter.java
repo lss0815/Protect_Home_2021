19f230f84da5bea2e3a69489f05ef5a4da467750
lss0815 <js815002@gmail.com>
2021-04-18 23:05:03 +0900

    android: add listview

--- android/app/src/main/java/com/skku/protecthome/ListViewAdapter.java
+++ android/app/src/main/java/com/skku/protecthome/ListViewAdapter.java
@@ -1,14 +1,57 @@
 package com.skku.protecthome;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListViewAdapter extends BaseAdapter {
-    priavete TextView dateTV;
+    private TextView dateTextView;
 
-    private ArrayList<ListViewItem>
+    private ArrayList<ListViewItem> listViewItemArrayList = new ArrayList<ListViewItem>();
 
     @Override
     public int getCount() {
-        return list
+        return listViewItemArrayList.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        Context context = parent.getContext();
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.listview_item, parent, false);
+        }
+
+        dateTextView = (TextView) convertView.findViewById(R.id.date);
+
+        ListViewItem listViewItem = listViewItemArrayList.get(position);
+
+        dateTextView.setText(listViewItem.getDate());
+
+        return convertView;
+    }
+
+    @Override
+    public long getItemId(int position){
+        return position;
+    }
+
+    @Override
+    public Object getItem(int position){
+        return listViewItemArrayList.get(position);
+    }
+
+    public void addItem(ListViewItem _item){
+        ListViewItem item = new ListViewItem();
+
+        item.setDate(_item.getDate());
+
+        listViewItemArrayList.add(item);
     }
 }
--- android/app/src/main/java/com/skku/protecthome/MainActivity.java
+++ android/app/src/main/java/com/skku/protecthome/MainActivity.java
@@ -3,12 +3,33 @@ package com.skku.protecthome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private ListViewAdapter adapter;
+    SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new ListViewAdapter();
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        ListViewItem item = new ListViewItem();
+        Date date = new Date();
+        item.setDate(dateformat.format(date));
+
+        adapter.addItem(item);
+
+        adapter.notifyDataSetChanged();
     }
 }
